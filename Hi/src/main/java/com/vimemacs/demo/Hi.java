fc132c0e8aa25e3113241068b0cce98320a683ba
VimIcewind <VimIcewind@gmail.com>
2021-03-04 14:43:20 +0800

    Hi, java!

---
+++ Hi/src/main/java/com/vimemacs/demo/Hi.java
@@ -0,0 +1,11 @@
+package com.vimemacs.demo;
+
+/**
+ * @author HWD
+ * @date 2021/3/4 14:38
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("com.vimemacs.demo.Hi, java!");
+    }
+}
